775b961cf442f981c5830440e450d95df0849c7d
mason <masonliu03@gmail.com>
2024-11-08 16:31:34 -0800

    added rooms and started function for first task

--- main.cc
+++ main.cc
@@ -1,23 +1,95 @@
-#include "colors.h" // If you want to make your map look pretty and use colors :)
+//#include "colors.h" // If you want to make your map look pretty and use colors :)
 #include <iostream>
 #include <string>
 #include <vector>
 #include <iomanip>
+using namespace std;
+
+
+//preprocesor
+
+void date();
 
 int main(){
- /* vector<vector<map>> char; // You can use a 2D vector if you'd like, but it's a lot better to emulate a 2D vector with a 1D one.
-cout << "Hello World" << endl;
-cout << "Someting" << endl;
-cout << "jkdsfjhkdsjhkgdsgjhk";
-a
+vector<string> room1, room2, room3, room4; 
+// You can use a 2D vector if you'd like, but it's a lot better to emulate a 2D vector with a 1D one.
+
+// for some reasons the rooms wont compile
+// needs looking into
+/*
+room1 = {
+
+    "------------------------------------------",
+    "|                                        |",
+    "|                                        |",
+    "|               (room 1)                 |",
+    "|                                        |",
+    "|                                        |",
+    "|                                        |",
+    "------------------------------------------",
+};
+room2 = {
+
+    "------------------------------------------",
+    "|                                        |",
+    "|                                        |",
+    "|               (room 2)                 |",
+    "|                                        |",
+    "|                                        |",
+    "|                                        |",
+    "------------------------------------------"};
+room3 = {
+
+    "------------------------------------------",
+    "|                                        |",
+    "|                                        |",
+    "|               (room 3)                 |",
+    "|                                        |",
+    "|                                        |",
+    "|                                        |",
+    "------------------------------------------",
+};
+room4 = {
+
+    "------------------------------------------",
+    "|                                        |",
+    "|                                        |",
+    "|               (room 4)                 |",
+    "|                                        |",
+    "|                                        |",
+    "|                                        |",
+    "------------------------------------------",
+};
+*/
+
+
 
 // This is where all your logic will go
-while (true)
+while (1 > 2)
 {
   // TODO: Get user input
 
   // TODO Move character or something cool using the user's input
 
   // TODO: Output map
-*/
-  }
+
+}
+date();
+}
+
+// Room 1 puzzles
+
+
+//
+void date() {
+  // mac users use clear and windows uses cls
+
+  // system("cls");
+
+  system("clear");
+
+  cout << setw(20)<< "The date is November 9, 2024. The day after the Engineering Renaissance assignment is due." << endl << right;
+  cout << right << setw(50) << " You wake up in a cold dark room with no doors or windows in sight." << endl;
+  cout << setw(75) << right << "Also you can suddenly fly.";
+}
+
