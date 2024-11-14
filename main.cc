059ce1c21d11975db4286713006806294ea34f46
isabelleong001 <isabelleong001@gmail.com>
2024-11-14 15:54:33 -0800

    Passcode verification added to room 1

--- main.cc
+++ main.cc
@@ -9,8 +9,12 @@ using namespace std;
 //preprocesor
 
 void date();
+void thing (){
+    cout << "whatever" << endl;
+}
 
 int main(){
+  thing();
 vector<string> room1, room2, room3, room4; 
 // You can use a 2D vector if you'd like, but it's a lot better to emulate a 2D vector with a 1D one.
 
@@ -74,7 +78,7 @@ while (1 > 2)
   // TODO: Output map
 
 }
-date();
+//date();
 }
 
 // Room 1 puzzles
@@ -90,6 +94,6 @@ void date() {
   // working on alligning this
   cout << setw(20)<< "The date is November 9, 2024. The day after the Engineering Renaissance assignment is due." << endl << right;
   cout << right << setw(50) << " You wake up in a cold dark room with no doors or windows in sight." << endl;
-  cout << setw(75) << right << "Also you can suddenly fly.";
+  cout << setw(75) << right << "Also you can suddenly fly. I can't explain that one yet.";
 }
 
--- room_1.cpp
+++ room_1.cpp
@@ -13,14 +13,17 @@ beisdes you robert requiring them
 #include <cmath>
 #include <iomanip>
 using namespace std;
+// There is only one puzzle in room one. The player needs to input the date (November 22,2024) as a reduced number to escape to room 2.
+
 
 // main function
 int main() {
+    thing();
     cout << "The date is November 22, 2024. The day the engineering Renaissance RPG is due\n";
     cout << "You wake up in a cold dark room with no doors or windows in sight. Also you can suddenly fly?\n";
     // why isnt my setw(n) working???
     //trying to somewhat center it with the longer lines of text without
-    cout << left << setw(15) << "I can't explain that one yet....\n\n\n";
+    cout << center << setw(15) << "I can't explain that one yet....\n\n\n";
     cout << "This RPG will use reducing numbers, so let's get familiar with it.\n";
     cout << "In order to reduce a number, add the sum of its digits.\n";
     cout << "If your answer still has more than one digit, then repeat this process until you 're left with a single digit.\n";
@@ -30,20 +33,30 @@ int main() {
 
     //varaible to hold users answer
     int answer;
-    cout << "Alright, now it’s your turn. Make your way over to the ! and put in today’s date. Good luck!";
-    cout << "\n\nwhat did you get?";
-    cout << "\nanswer: ";
-    cin >> answer;
+    cout << "Alright, now it's your turn. Make your way over to the ! and put in today's date. Good luck!";
 
-    //come back and set a real answer later
-    if (answer = /* the correct answer */) {
-        cout << "correct";
-    }
-    else {
-        while (answer != /* the correct answer */) {
-            cout << "try again";
-            cin >> answer;
-        }
-        cout << "you finally got it correct";
-    }
-}
+    
+void door_1 (){
+    cout << "If you need to go back and look at the puzzles, enter 0 to cancel.\n";
+int passcode1;
+cout << "Please enter the passcode:\n";
+cin >> passcode_1;
+while(true) {
+	if (passcode_1 > 9) {
+		cout << "The passcode is a one digit number.\n";
+		cout << "Please try again.\n";
+		}
+	else if (passcode_1 = 5) {
+		cout << "The room goes dark for a second.\n";
+		// switch map from Room 1 to Room 2
+        cout << "When the light returns, you find that the decorations in the room have changed, or perhaps you're in a different room with identical architecture. Either way, it doesn't matter. You're still trapped.\n";
+		break;
+		}
+	else if (passcode_1 = 0) {
+		break;
+		}
+	else {
+		cout << "Please try again.\n";
+		}
+}    
+}}
