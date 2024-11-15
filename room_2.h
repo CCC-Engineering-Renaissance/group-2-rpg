f194d1ccab0d068e9520b84031dcf752badb181f
mason-042 <masonliu03@gmail.com>
2024-11-14 17:08:40 -0800

    Update room_2.h
    tbis is better tah twhat u do isaabelle

--- room_2.h
+++ room_2.h
@@ -2,9 +2,8 @@
 #include <cmath>
 #include <iomanip>
 using namespace std;
-int main() {
 
-}
+
 void puzzle_2_1() {
 cout  << "You see a lovely portrait of a king, queen, and their two sons. One son is just a baby, but the other one looks much older.\n"; 
 cout  << "Underneath the portrait, it says that both sons are named Ace.";
@@ -16,15 +15,15 @@ int people;
 int peoplemissing;
 cout << "It's an oval shaped mirror.\n";
 cout << "You stare a little too long, and the mirror whispers to you, \"How many people are in this room right now?\".";
-cin >> people;
 while(true) {
-	if (people = 0) {
+    cin >> people;
+	if (people == 0) {
 	cout << "Not even gonna count yourself? Try that again.\n";
 		}
-	else if (people = 1) {
+	else if (people == 1) {
 	cout << "Not in the game silly. Take your eyes off the screen. Look around you.\n";
 		}
-	else if (people = 34) {
+	else if (people == 34) {
   cout << "Excellent work! Try not to forget that number. Come see yourself again if you do.\n";
   break;
   }
@@ -48,23 +47,31 @@ cout << "When entering these digits in the passcode, you will need to enter them
 cout << "their number entered first. The second leftmost puzzle will have their number entered next, and so on and so forth. Now before you ask about that one\n";
 cout << "puzzle in that one odd location, that one can be entered last.\n";
 cout << "If you need to go back and look at the puzzles, enter 0 to cancel.\n";
-cout << "Please enter the passcode:\n";
-cin >> passcode_2;
+cout << "Please enter the passcode: ";
 while(true) {
+    cin >> passcode_2;
 	if (passcode_2 > 999 || passcode_2 < 100) {
 		cout << "The passcode is a three digit number.\n";
-		cout << "Please try again.\n";
+		cout << "Please try again: ";
 		}
-	else if (passcode_2 = 574) {
+	else if (passcode_2 == 574) {
 		cout << "The room goes dark for a second.\n";
 		// switch map from Room 2 to Room 3
 cout << "Well uh, you probably know the drill by now. The room looks different! Also you're still trapped. Sorry about that.\n";
 		break;
 		}
-	else if (passcode_2 = 0) {
+	else if (passcode_2 == 0) {
 		break;
 		}
 	else {
-		cout << "Please try again.\n";
+		cout << "Please try again: ";
 		}
 }}
+
+int main()
+{
+    puzzle_2_1();
+    puzzle_2_2();
+    puzzle_2_3();
+    door_2();
+}
