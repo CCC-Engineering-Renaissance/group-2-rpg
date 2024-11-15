0dae0b26bb618634fe1708bb9d08165ce07496b9
isabelleong001 <isabelleong001@gmail.com>
2024-11-15 15:34:12 -0800

    Create room_3.h

---
+++ room_3.h
@@ -0,0 +1,6 @@
+// This is the first puzzle in room 3. It requires you to use a hex code. The answer for this puzzle is 4.
+void puzzle_3_1() {
+  cout << "It’s a rose! What color is it?\n";
+  cout << RED << "00FF00\n" << RESET;
+  cout << "For the passcode input the color’s spot in the rainbow (e.g. red = 1, violet =7).\n";
+}
