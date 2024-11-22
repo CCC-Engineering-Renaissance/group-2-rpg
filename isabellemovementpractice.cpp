4cd73072a9f2b3cf33ff51dfcae85fa35cec1d4c
isabelleong001 <isabelleong001@gmail.com>
2024-11-22 13:27:23 -0800

    Update isabellemovementpractice.cpp

--- isabellemovementpractice.cpp
+++ isabellemovementpractice.cpp
@@ -6,9 +6,11 @@
 #include <cstdlib>
 #include <ctime>
 #include"colors.h"
+#include"room_1.h"
 using namespace std;
 int main() {
 vector<string> room1;
+vector<string> room1_edited;
 room1 = {
 
 "------------------------------------------\n",
@@ -23,71 +25,132 @@ room1 = {
 
 int current_column = 2;
 int current_row = 6;
-room1[current_row][current_column] ='&';
-for (int i = 0; i < room1.size(); i++)
+room1_edited = room1;
+room1_edited[current_row][current_column] ='&';
+for (int i = 0; i < room1_edited.size(); i++)
 {
-cout << room1[i] << "";}
+cout << room1_edited[i] << "";}
+set_raw_mode(true);
 while (true){
 char player_movement;
-cin >> player_movement;
+player_movement = quick_read();
 char c = toupper(player_movement);
 if (c == 'W') {
-room1[current_row][current_column] =' ';
 current_row = current_row - 1;
-}
-else if (c == 'A') {
-room1[current_row][current_column] =' ';
-current_column = current_column - 1;
-}
-else if (c == 'S') {
-room1[current_row][current_column] =' ';
-current_row = current_row + 1;
-}
-else if (c == 'D') {
-room1[current_row][current_column] =' ';
-current_column = current_column + 1;
-}
-if(current_row == 0){
-    room1[current_row][current_column] = '-';
+    if(current_row == 0){
+    room1_edited[current_row][current_column] = '-';
     current_row = current_row + 1;
 }
 else if(current_row == 7){
-    room1[current_row][current_column] = '-';
+    room1_edited[current_row][current_column] = '-';
     current_row = current_row - 1;
 }
 else if(current_column == 0){
-    room1[current_row][current_column] = '|';
+    room1_edited[current_row][current_column] = '|';
     current_column = current_column + 1;
 }
 else if(current_column == 41){
-    room1[current_row][current_column] = '|';
+    room1_edited[current_row][current_column] = '|';
     current_column = current_column - 1;
 }
-if(!(current_row == 4 && current_column == 10)){
-    room1[4][10] = '#';
-}
-if(!(current_row == 5 && current_column == 25)){
-    room1[5][25] = '2';
-}
-if(!(current_row == 5 && current_column == 27)){
-    room1[5][27] = '2';
-}
-if(!(current_row == 5 && current_column == 26)){
-    room1[5][26] = '0';
-}
-if(!(current_row == 5 && current_column == 28)){
-    room1[5][28] = '4';
-}
-if(!(current_row == 6 && current_column == 35)){
-    room1[6][35] = '!';
-}
-cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-room1[current_row][current_column] ='&';
-for (int i = 0; i < room1.size(); i++)
+room1_edited = room1;
+system("clear");
+room1_edited[current_row][current_column] ='&';
+for (int i = 0; i < room1_edited.size(); i++)
 {
-cout << room1[i] << "";}
+cout << room1_edited[i] << "";}
+
 }
+else if (c == 'A') {
+current_column = current_column - 1;
+    if(current_row == 0){
+    room1_edited[current_row][current_column] = '-';
+    current_row = current_row + 1;
 }
+else if(current_row == 7){
+    room1_edited[current_row][current_column] = '-';
+    current_row = current_row - 1;
+}
+else if(current_column == 0){
+    room1_edited[current_row][current_column] = '|';
+    current_column = current_column + 1;
+}
+else if(current_column == 41){
+    room1_edited[current_row][current_column] = '|';
+    current_column = current_column - 1;
+}
+room1_edited = room1;
+system("clear");
+room1_edited[current_row][current_column] ='&';
+for (int i = 0; i < room1_edited.size(); i++)
+{
+cout << room1_edited[i] << "";}
+
+}
+else if (c == 'S') {
+current_row = current_row + 1;
+    if(current_row == 0){
+    room1_edited[current_row][current_column] = '-';
+    current_row = current_row + 1;
+}
+else if(current_row == 7){
+    room1_edited[current_row][current_column] = '-';
+    current_row = current_row - 1;
+}
+else if(current_column == 0){
+    room1_edited[current_row][current_column] = '|';
+    current_column = current_column + 1;
+}
+else if(current_column == 41){
+    room1_edited[current_row][current_column] = '|';
+    current_column = current_column - 1;
+}
+room1_edited = room1;
+system("clear");
+room1_edited[current_row][current_column] ='&';
+for (int i = 0; i < room1_edited.size(); i++)
+{
+cout << room1_edited[i] << "";}
+
+}
+else if (c == 'D') {
+current_column = current_column + 1;
+    if(current_row == 0){
+    room1_edited[current_row][current_column] = '-';
+    current_row = current_row + 1;
+}
+else if(current_row == 7){
+    room1_edited[current_row][current_column] = '-';
+    current_row = current_row - 1;
+}
+else if(current_column == 0){
+    room1_edited[current_row][current_column] = '|';
+    current_column = current_column + 1;
+}
+else if(current_column == 41){
+    room1_edited[current_row][current_column] = '|';
+    current_column = current_column - 1;
+}
+room1_edited = room1;
+system("clear");
+room1_edited[current_row][current_column] ='&';
+for (int i = 0; i < room1_edited.size(); i++)
+{
+cout << room1_edited[i] << "";}
+
+}
+
+if(c == 'E'){
+    if((current_row == 4 && current_column == 10)){
+    retutorial();
+}
+    else if((current_row == 5) && (current_column == 25 || current_column == 26 || current_column == 27 || current_column ==28)){
+    calendar();
+    }
+    else if((current_row == 6 && current_column == 35)){
+    door_1();
+}}
+}}
 
 
 
