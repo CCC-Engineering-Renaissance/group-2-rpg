1421bdd6ad12448b7131a719f79c990978d052bb
isabelleong001 <isabelleong001@gmail.com>
2024-11-22 15:15:27 -0800

    Update room_3.h

--- room_3.h
+++ room_3.h
@@ -1,23 +1,25 @@
-#pragma once
+
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+#include"colors.h"
 using namespace std;
-
+int choice;
 // This is the first puzzle in room 3. It requires you to use a hex code. The answer for this puzzle is 1.
 void puzzle_3_1() {
-  int choice;
+  set_raw_mode(false);
   cout << "It's a rose! What color is it?";
   cin.get();
   cout << "FF0000";
   cin.get();
-  cout << "For the passcode input the color's spot in the rainbow (e.g. red = 1, orange = 2, yellow = 3).";
+  cout << "For the passcode input the color's spot in the rainbow (e.g. red = 1, violet = 7).";
   cin.get();
+  set_raw_mode(true);
 }
 
 // This is the second puzzle in room 3. The number is hidden in the first letter of every line. The answer for this puzzle is 9.
 void puzzle_3_2() {
-// the couts here tell story
+  set_raw_mode(false);
   cout << "You press a button and a speaker turns on.";
   cin.get();
   cout << "The voice is distorted.";
@@ -30,11 +32,12 @@ void puzzle_3_2() {
   cin.get();
   cout << "\"Except you need to figure out this one first.\"";
   cin.get();
+  set_raw_mode(true);
 }
 
 // This is the third puzzle in room 3. It refers to the term "snake eyes" in dice. The answer for this puzzle is 2.
 void puzzle_3_3() {
-  int choice;
+  set_raw_mode(false);
   cout << "It's a cardboard box.";
   cin.get();
   cout << "Open it?";
@@ -56,9 +59,11 @@ cin >> choice;
     cin.get();
     cout << "But you continue to wonder about its contents...";
     cin.get();
-}}
+}set_raw_mode(true);
+}
 
-void door_3(){
+bool door_3(){
+  set_raw_mode(false);
 int passcode_3;
 cout << "Don't forget to enter the leftmost puzzle answer first.";
 cin.get(); 
@@ -67,11 +72,7 @@ cin.get();
 cout << "Please enter the passcode: ";
 while(true) {
     cin >> passcode_3;
-	if (passcode_3 > 999 || passcode_3 < 100) {
-		cout << "The passcode is a three digit integer.\n";
-		cout << "Please try again: ";
-		}
-	else if (passcode_3 == 192) {
+	if (passcode_3 == 192) {
 		cout << "The room goes dark for a second.\n";
 		cin.get();
 		// switch map from Room 3 to Room 4
@@ -81,12 +82,17 @@ cout << "You're still trapped though.";
 cin.get(); 
 cout << "Sorry again.";
 cin.get();
-		break;
+		return 1;
 		}
 	else if (passcode_3 == 0) {
-		break;
+		return 0;
+		}
+    if (passcode_3 > 999 || passcode_3 < 100) {
+		cout << "The passcode is a three digit integer.\n";
+		cout << "Please try again: ";
 		}
 	else {
 		cout << "Please try again: ";
 		}
-}};
+} set_raw_mode(true);
+};
